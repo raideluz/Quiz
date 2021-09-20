3d6b524f1dbf319886869ea38c81b8501aafd896
Natália Carolina Dos Santos <nataliacarolina@MacBook-Pro-de-Natalia.local>
2021-09-19 23:03:20 -0300

    Commit inicial

---
+++ Quiz/Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quiz
+//
+//  Created by Natália Carolina Dos Santos on 19/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let viewController = HomeViewController()
+        
+        
+        return true
+    }
+
+
+}
+
